351affd75f0a20d4e08052d2a3cfeb288c69cfad
Thomas Kildahl <69842791+ThomasK97@users.noreply.github.com>
2021-09-21 13:11:34 +0200

    heiho

---
+++ js/java.js
@@ -0,0 +1,16 @@
+const multiply =(num1, num2) => {
+    return num1 + num2;
+};
+
+const sum1 = (num1, num2) => {
+    return num1 + num2;
+};
+
+const division = (num1, num2) => {
+    return num1 / num2;
+};
+
+console.log(
+    sum1(multiply(3,9),
+    division(13,6))
+);
